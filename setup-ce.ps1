97a1673a385ee6a70d8184eaefddfaa0329db0b5
partouf <partouf@gmail.com>
2022-12-28 17:15:26 +0100

    try to limit diskspace

--- setup-ce.ps1
+++ setup-ce.ps1
@@ -11,4 +11,15 @@ npm run ts-compile
 
 xcopy "c:\compilerexplorer\out\webpack\static" "c:\compilerexplorer\out\dist\static"
 
+Remove-Item -Path "c:\compilerexplorer\out\webpack" -Recurse
+Remove-Item -Path "c:\compilerexplorer\node_modules" -Recurse
+
+setx NODE_ENV production
+
+npm i --no-audit --ignore-scripts --production
+
+Remove-Item -Path "node_modules/.cache" -Recurse
+Remove-Item -Path "node_modules/monaco-editor" -Recurse
+Remove-Item -Path "node_modules" -Include "*.ts" -Recurse
+
 node -r esm out/dist/app.js --version --dist
